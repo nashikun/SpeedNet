fbc7679e9826f2f386710260fd66adfc9c583033
Anass Elidrissi <anasselidrissi97@gmail.com>
2020-08-29 22:00:19 +0800

    Making the Screen class a singleton

--- game/display/screen.py
+++ game/display/screen.py
@@ -1,7 +1,16 @@
 from queue import PriorityQueue
 
 
-class Screen:
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class Screen(metaclass=Singleton):
     game_objects = PriorityQueue()
 
     def __init__(self, height, width):
@@ -11,7 +20,3 @@ class Screen:
     def render(self):
         for game_object in self.game_objects:
             game_object.item.render()
-
-    def get_screen(self):
-        # Should return a reference to the screen object (tktinter's for example)
-        return None
