54143a01631c83d3e792da0aa95e2519f95194ed
Anass Elidrissi <anasselidrissi97@gmail.com>
2020-08-29 23:42:14 +0800

    Separating the init into different parts

--- game/game.py
+++ game/game.py
@@ -4,24 +4,40 @@ from queue import Queue
 from game.display.screen import Screen
 import time
 
+from game.player.player import Player
+
 
 class Game:
-    def __init__(self, level_type="CellularAutomataLevel", level_args={"h": 10, "w": 10}, screen_height=100,
-                 screen_width=100, ticks=None, render=True):
+    def __init__(self, level_type="CellularAutomataLevel", ticks=None, render=True, max_turns=None):
         self.level = getattr(importlib.import_module("game.levels"), level_type)()
-        self.level.init_map(**level_args)
-        self.screen = Screen(screen_height, screen_width)
+        self.objects = Queue()
         self.ticks = ticks
         self.render = render
-        self.objects = Queue()
+        self.max_turns = max_turns
+        self.turn = 0
+        self.screen = None
+
+    def init_screen(self, screen_height, screen_width):
+        self.screen = Screen(screen_height, screen_width)
+
+    def init_level(self, level_height=100, level_width=100, level_path=None):
+        self.level.init_map(level_height=level_height, level_width=level_width, level_path=level_path)
+
+    def init_player(self):
+        self.objects.put(Player())
 
     def run(self):
-        while True:
+        t = time.time()
+        while not self.is_over():
             if self.ticks:
-                t = time.time()
                 if time.time() - t < 1 / self.ticks:
                     continue
+            t = time.time()
             for obj in self.objects:
                 obj.run()
             if self.ticks and self.render:
                 self.screen.render()
+            self.turn += 1
+
+    def is_over(self):
+        return self.max_turns is not None and self.turn >= self.max_turns
---
+++ main.py
@@ -0,0 +1,43 @@
+import argparse
+import json
+from game.game import Game
+
+
+def init_parser():
+    parser = argparse.ArgumentParser(description='Runs the project')
+    subparsers = parser.add_subparsers(help='sub-command help', dest='command')
+
+    file_parser = subparsers.add_parser('file', help='Reads the options from a config file')
+    file_parser.add_argument("--config_path", type=str, required=True)
+
+    manual_parser = subparsers.add_parser('manual', help='Enters the options manually')
+    manual_parser.add_argument("--level_type", type=str, default="CellularAutomataLevel")
+    manual_parser.add_argument("--level_path", type=str, default=None)
+    manual_parser.add_argument("--level_width", type=int, default=100)
+    manual_parser.add_argument("--level_height", type=int, default=100)
+    manual_parser.add_argument("--screen_height", type=int, default=100)
+    manual_parser.add_argument("--screen_width", type=int, default=100)
+    manual_parser.add_argument("--ticks", type=int, default=1)
+    manual_parser.add_argument("--max_turns", type=int, default=None)
+    return parser
+
+
+if __name__ == "__main__":
+    parser = init_parser()
+    args = parser.parse_args()
+    if args.command == "file":
+        with open(args.config_path, 'r') as f:
+            config = json.load(f)
+    else:
+        config = vars(args)
+
+    game_config = {"level_type": config["level_type"], "render": config["render"], "ticks": config["ticks"],
+                   "max_turns": config["max_turns"]}
+    screen_config = {"screen_height": config["screen_height"], "screen_width": config["screen_width"]}
+    level_config = {"level_height": config["level_height"], "level_width": config["level_width"],
+                    "level_path": config["level_path"]}
+    model_config = {}
+    game = Game(**game_config)
+    game.init_screen(**screen_config)
+    game.init_level(**level_config)
+    game.run()
